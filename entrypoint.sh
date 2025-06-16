93fab00084e4a4fb7befeed31e5eb5354092369f
gu3gu3 <gueeguense@gmail.com>
2025-06-16 11:21:54 -0600

    fix: Fetch secret explicitly in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Fetch the database URL from Secret Manager and export it
+export DATABASE_URL=$(gcloud secrets versions access latest --secret=DATABASE_URL --project=digital-menu-455517)
+
 # Navigate to the backend directory where prisma and the app source are
 cd packages/backend
 
