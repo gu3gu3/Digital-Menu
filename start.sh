dcaa40bb11a772f7ae9f69ac4ec8b29f9fbe82ea
gu3gu3 <gueeguense@gmail.com>
2025-06-18 09:40:45 -0600

    feat(deploy): Despliegue Con GCS Configurado

--- start.sh
+++ start.sh
@@ -2,10 +2,9 @@
 set -e
 
 # Iniciar el servidor de Node en segundo plano.
-# Gracias a la variable NODE_ENV=production, escuchará en el puerto 3001.
 echo "Iniciando servidor de Node.js..."
 node dist/src/index.js &
 
-# Iniciar Nginx en primer plano, que escuchará en el puerto 8080.
+# Iniciar Nginx en primer plano.
 echo "Iniciando Nginx..."
 exec nginx -g 'daemon off;' 
