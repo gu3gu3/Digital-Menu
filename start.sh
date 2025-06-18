1ff0fdf7793b61e769b4b6eeb0a9d63498a811b7
gu3gu3 <gueeguense@gmail.com>
2025-06-18 09:26:19 -0600

    feat(deploy): Despliegue para validacion de env de Node

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "============================================================"
-echo "MODO DE DEPURACIÓN FINAL: EJECUTANDO SOLO EL BACKEND"
-echo "Se intentará iniciar el servidor de Node.js en primer plano."
-echo "Nginx no se iniciará. Se espera que la UI no cargue."
-echo "Buscando logs del servidor Node.js a continuación..."
-echo "============================================================"
+# Iniciar el servidor de Node en segundo plano.
+# Gracias a la variable NODE_ENV=production, escuchará en el puerto 3001.
+echo "Iniciando servidor de Node.js..."
+node dist/src/index.js &
 
-# Ejecutar Node.js en primer plano para capturar todos los logs
-node dist/src/index.js 
+# Iniciar Nginx en primer plano, que escuchará en el puerto 8080.
+echo "Iniciando Nginx..."
+exec nginx -g 'daemon off;' 
