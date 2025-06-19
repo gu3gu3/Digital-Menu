8fdf3e832029b8d4f7acc0c62d26ddb4a9e9ba6d
gu3gu3 <gueeguense@gmail.com>
2025-06-19 09:42:19 -0600

    Fix(cd):Condicion de Carrera Solucion

--- start.sh
+++ start.sh
@@ -5,6 +5,13 @@ set -e
 echo "Iniciando servidor de Node.js..."
 node dist/src/index.js &
 
-# Iniciar Nginx en primer plano.
+# Esperar a que el backend de Node.js esté listo en el puerto 3001
+echo "Esperando a que el backend esté listo en el puerto 3001..."
+while ! nc -z localhost 3001; do   
+  sleep 1 # esperar 1 segundo antes de reintentar
+done
+echo "✅ Backend listo y escuchando en el puerto 3001."
+
+# Iniciar Nginx en primer plano, ahora que el backend está listo.
 echo "Iniciando Nginx..."
 exec nginx -g 'daemon off;' 
